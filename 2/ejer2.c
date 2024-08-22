77220dd4886f86e151f6cf7aa2c972c36ac43505
YOUR_NAME <lregaladofloriano@gmail.com>
2024-08-22 16:04:33 -0600

    Changes to be committed: 	modified:   ejer2.c

--- 2/ejer2.c
+++ 2/ejer2.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h> 
 #include <unistd.h>
@@ -6,12 +7,11 @@ int main(int argc, char *argv[])
 {
   unsigned int n;
   scanf("%d",&n);
-  for (unsigned int i=0; i<n; i++){
-    pid_t p = fork();
-    printf("%d\n",i);
-    if (p !=0) {
-      wait(NULL);
-    }
-    exit(1);
-  }
+  int i = n;
+  if (i<=0) return 1;
+  a:
+  pid_t p = fork();
+  printf("%d\n",i);
+  i--;
+  if (i!=0) goto a;
 }
