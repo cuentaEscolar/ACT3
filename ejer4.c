b06e76eddec172d028cca06ac199c727f6cccc47
YOUR_NAME <lregaladofloriano@gmail.com>
2024-08-25 21:31:03 -0600

    Changes to be committed: 	modified:   ejer4.c 	new file:   helper.c

---
+++ ejer4.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h> 
+#include <unistd.h>
+#define CUSTOMEXIT 5
+int main(int argc, char *argv[])
+{
+  unsigned int n;
+  scanf("%d",&n);
+  unsigned i = n;
+  if (i==0) return 0;
+  n++;
+  back:
+    pid_t p = fork();
+    pid_t cpid;
+
+    if (p==0) exit(CUSTOMEXIT);
+    cpid = wait(NULL);
+
+    printf("Proceso hijo %d\n", n -i);
+    
+    i--;
+  if (i!=0) goto back;
+  printf("Fin\n");
+}
